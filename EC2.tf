95f20a5adb4ef44975c2e10ff67df3857da61c15
sallyshin <tlstpwjdssj@gmail.com>
2022-06-27 18:14:34 +0900

    EC2 resource

--- EC2.tf
+++ EC2.tf
@@ -31,30 +31,30 @@ resource "aws_instance" "global-public-ec2-c" {
 }
 # AutoScaling Group 시작구성으로 자동구성
 # #web
-# resource "aws_instance" "global-private-ec2-a-web" {
-#   ami               = var.ec2_ami
-#   instance_type     = "t2.micro"
-#   availability_zone = var.az_a
+resource "aws_instance" "global-private-ec2-a-web" {
+  ami               = var.ec2_ami
+  instance_type     = "t2.micro"
+  availability_zone = var.az_a
 
-#   subnet_id = aws_subnet.global-private-subnet-a-web.id
-#   vpc_security_group_ids = [
-#     aws_security_group.global-private-sg-web.id
-#   ]
-#   tags = {
-#     Name = "global-private-ec2-a-web"
-#   }
-# }
+  subnet_id = aws_subnet.global-private-subnet-a-web.id
+  vpc_security_group_ids = [
+    aws_security_group.global-private-sg-web.id
+  ]
+  tags = {
+    Name = "global-private-ec2-a-web"
+  }
+}
 
-# resource "aws_instance" "global-private-ec2-c-web" {
-#   ami               = var.ec2_ami
-#   instance_type     = "t2.micro"
-#   availability_zone = var.az_c
+resource "aws_instance" "global-private-ec2-c-web" {
+  ami               = var.ec2_ami
+  instance_type     = "t2.micro"
+  availability_zone = var.az_c
 
-#   subnet_id = aws_subnet.global-private-subnet-c-web.id
-#   vpc_security_group_ids = [
-#     aws_security_group.global-private-sg-web.id
-#   ]
-#   tags = {
-#     Name = "global-private-ec2-c-web"
-#   }
-# }
+  subnet_id = aws_subnet.global-private-subnet-c-web.id
+  vpc_security_group_ids = [
+    aws_security_group.global-private-sg-web.id
+  ]
+  tags = {
+    Name = "global-private-ec2-c-web"
+  }
+}
